ae25e6469a177a3177103b0dc654c45008e097d8
Kathy Gray <kathryngray@fb.com>
2017-03-16 18:41:16 +0000

    Get variable declarations supported

--- src/evaluators/VariableDeclaration.js
+++ src/evaluators/VariableDeclaration.js
@@ -15,6 +15,7 @@ import type { Value } from "../values/index.js";
 import type { Reference } from "../environment.js";
 import { ObjectValue, StringValue } from "../values/index.js";
 import {
+  BindingInitialization,
   PutValue,
   GetValue,
   ResolveBinding,
@@ -24,54 +25,67 @@ import {
   SetFunctionName,
 } from "../methods/index.js";
 import invariant from "../invariant.js";
-import type { BabelNodeVariableDeclaration } from "babel-types";
+import type { BabelNodeVariableDeclaration, BabelNodeIdentifier } from "babel-types";
 
 // ECMA262 13.3.1.4
 function letAndConst (ast: BabelNodeVariableDeclaration, strictCode: boolean, env: LexicalEnvironment, realm: Realm): Value | Reference {
   for (let declar of ast.declarations) {
-    if (declar.id.type !== "Identifier") {
-      throw new Error("TODO: Patterns aren't supported yet");
-    }
-
     let Initializer = declar.init;
-    if (!Initializer) {
 
-      // 1. Let lhs be ResolveBinding(StringValue of BindingIdentifier).
-      let bindingId = declar.id.name;
-      let lhs = ResolveBinding(realm, bindingId, strictCode);
+    if (declar.id.type == "Identifier") {
+      let id = ((declar.id: any): BabelNodeIdentifier);
 
-      // 2. Return InitializeReferencedBinding(lhs, undefined).
-      InitializeReferencedBinding(realm, lhs, realm.intrinsics.undefined);
-      continue;
-    }
+      if (!Initializer) {
 
-    // 1. Let bindingId be StringValue of BindingIdentifier.
-    let bindingId = declar.id.name;
+        // 1. Let lhs be ResolveBinding(StringValue of BindingIdentifier).
+        let bindingId = id.name;
+        let lhs = ResolveBinding(realm, bindingId, strictCode);
 
-    // 2. Let lhs be ResolveBinding(bindingId).
-    let lhs = ResolveBinding(realm, bindingId, strictCode);
+        // 2. Return InitializeReferencedBinding(lhs, undefined).
+        InitializeReferencedBinding(realm, lhs, realm.intrinsics.undefined);
+        continue;
+      }
 
-    // 3. Let rhs be the result of evaluating Initializer.
-    let rhs = env.evaluate(Initializer, strictCode);
+      // 1. Let bindingId be StringValue of BindingIdentifier.
+      let bindingId = id.name;
 
-    // 4. Let value be ? GetValue(rhs).
-    let value = GetValue(realm, rhs);
+     // 2. Let lhs be ResolveBinding(bindingId).
+     let lhs = ResolveBinding(realm, bindingId, strictCode);
 
-    // 5. If IsAnonymousFunctionDefinition(Initializer) is true, then
-    if (IsAnonymousFunctionDefinition(realm, Initializer)) {
-      invariant(value instanceof ObjectValue);
+     // 3. Let rhs be the result of evaluating Initializer.
+     let rhs = env.evaluate(Initializer, strictCode);
+
+     // 4. Let value be ? GetValue(rhs).
+     let value = GetValue(realm, rhs);
+
+     // 5. If IsAnonymousFunctionDefinition(Initializer) is true, then
+     if (IsAnonymousFunctionDefinition(realm, Initializer)) {
+       invariant(value instanceof ObjectValue);
 
       // a. Let hasNameProperty be ? HasOwnProperty(value, "name").
       let hasNameProperty = HasOwnProperty(realm, value, "name");
 
       // b. If hasNameProperty is false, perform SetFunctionName(value, bindingId).
       if (!hasNameProperty) SetFunctionName(realm, value, new StringValue(realm, bindingId));
-    }
+     }
 
-    // 6. Return InitializeReferencedBinding(lhs, value).
-    InitializeReferencedBinding(realm, lhs, value);
+     // 6. Return InitializeReferencedBinding(lhs, value).
+     InitializeReferencedBinding(realm, lhs, value);
+   } else { //BindingPattern
+     // 1. Let rhs be the result of evaluating Initializer.
+     let rhs = Initializer ? env.evaluate(Initializer, strictCode)
+              : realm.intrinsics.undefined;
+
+     // 2. Let value be ? GetValue(rhs).
+     let value = GetValue(realm, rhs);
+
+     // 3. Let env be the running execution context's LexicalEnvironment.
+     let binder_env = realm.getRunningContext().lexicalEnvironment;
+     
+     // 4. Return the result of performing BindingInitialization for BindingPattern using value and env as the arguments.
+     BindingInitialization(realm, declar.id, value, binder_env);
+   }
   }
-
   return realm.intrinsics.empty;
 }
 
@@ -83,7 +97,6 @@ export default function (ast: BabelNodeVariableDeclaration, strictCode: boolean,
 
   for (let declar of ast.declarations) {
     if (declar.id.type !== "Identifier") {
-      console.log("VariableDeclaration eval about to die with " + declar.id.type);
       throw new Error("TODO: Patterns aren't supported yet");
     }
 
